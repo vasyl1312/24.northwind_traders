87b3e25fd69b4dc1dc2162af5821bfc173101a2c
vasyl1312 <vasylhryts@knu.ua>
2023-06-22 16:44:57 +0300

    create swagger

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,26 @@
 import cors from 'cors'
 import express from 'express'
 import productsRoutes from './routes/productsRoutes'
+import { swaggerRouter } from './swagger/router'
+
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 const PORT = process.env.PORT || 8081
+const swaggerPort = process.env.SWAGGER_PORT || 4000
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/products', productsRoutes)
+app.use('/api-docs', swaggerRouter)
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
+})
+
+app.listen(swaggerPort, () => {
+  console.log(`Swagger listening on port  ${swaggerPort}`)
 })
